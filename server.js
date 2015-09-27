4051fb2f8f0860632d819da6599abdc8a3ce7b58
Travis Johnson <tjohnson01@gmail.com>
2015-09-27 11:45:29 -0600

    Test change

--- server.js
+++ server.js
@@ -15,7 +15,7 @@ var middleware = {
 app.use(middleware.logger);
 
 app.get('/about', middleware.requireAuthentication, function(req, res){
-    res.send('About Page!');
+    res.send('About Page!!');
 });
 
 app.use(express.static(__dirname + '/public'));
